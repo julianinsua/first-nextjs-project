114aa83b26890cdb4e5947a67237f99d6f65ca73
Julian Insua <jii.julious@gmail.com>
2020-11-30 03:59:12 -0300

    Created index page and auth page. Added links and router to index page

---
+++ pages/auth/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthIndexPage = () => {
+	return (
+		<div>
+			<h1>The Auth Index Page</h1>
+		</div>
+	);
+};
+
+export default AuthIndexPage;
---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Link from "next/link";
+import Router from "next/router";
+
+const indexPage = () => {
+	return (
+		<div>
+			<h1>The Main Page</h1>
+			<p>
+				Go to the{" "}
+				<Link href='/auth'>
+					<a>Auth</a>
+				</Link>
+				<button onClick={() => Router.push("/auth")}>Go to Auth</button>
+			</p>
+		</div>
+	);
+};
+
+export default indexPage;
